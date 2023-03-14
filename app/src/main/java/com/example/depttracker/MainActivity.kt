7a06c7af9154665351513029165b1dd8ab967d93
IdrisFallout <waithakasam2017@gmail.com>
2023-03-14 18:58:44 +0300

    creating app theme

--- app/src/main/java/com/example/depttracker/MainActivity.kt
+++ app/src/main/java/com/example/depttracker/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.depttracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // Delay for 2 seconds before starting the main activity
+        Handler().postDelayed({
+            setContentView(R.layout.activity_main)
+        }, 2000)
     }
 }
